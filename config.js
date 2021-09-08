a71bebbb8ef89ad1d92f19cf28ef196b05eaf789
Marc Zeller <pierarte@gmail.com>
2021-09-08 08:44:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ exports.config = {
     provider : 'https://polygon-mainnet.g.alchemy.com/v2/eCOapPZ9mpejAfalYFI1QYiM-3yfiULC',
     // tokens smart-contracts address
     diamondAddress : '0x86935f11c86623dec8a25696e1c19a8659cbf95d',
-    myAddress : '0x52Af26561a1E3baEeB0DF480dD43c5E587a666f9'
+    myAddress : ''
 
 
 }
