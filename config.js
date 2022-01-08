a8623d44d5945f3b8e4a4bd142f75fe0673c4349
Marc Zeller <pierarte@gmail.com>
2022-01-08 16:59:20 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ exports.config = {
     // this is why you don't want your config file shared publicly, make sure this script run somewhere safe
     password : '',
     // this can be an infura endpoint or your own node.
-    //provider : 'https://mainnet.infura.io/v3/a3456c11324745b7966ca0208ca8de4b',
+    //provider : '',
     provider : '',
     // tokens smart-contracts address
     diamondAddress : '0x86935f11c86623dec8a25696e1c19a8659cbf95d',
